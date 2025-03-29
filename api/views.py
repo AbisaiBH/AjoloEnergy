fdd743fef0805dbf444dc390f073a0f323775f01
abisai <abizabh@gmail.com>
2025-03-28 19:49:21 -0600

    Design_bd

--- api/views.py
+++ api/views.py
@@ -7,9 +7,7 @@ from decimal import Decimal
 
 @csrf_exempt
 def article_list_json(request):
-    # Obtenemos los artículos y seleccionamos los campos deseados
     articles = list(Article.objects.all().values('id', 'name', 'consumo_actual', 'consumo_estimado_mensual'))
-    # Retornamos los datos en formato JSON. 'safe=False' permite retornar una lista.
     return JsonResponse(articles, safe=False)
 
 @csrf_exempt
